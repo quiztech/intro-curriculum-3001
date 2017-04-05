73c1d977ba14f99609e67f8ba21d2b0326263144
quiztech <quiztechoverdrive@gmail.com>
2017-04-05 13:51:57 +0000

    解答

--- app.js
+++ app.js
@@ -6,8 +6,10 @@
  * @returns {Number}
  */
 function factorial(n) {
-    let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    let result = 1
+    for(let i=1; i <= n; i++) {
+        result *= i;
+    }
     return result;
 }
 
